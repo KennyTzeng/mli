5be536a60ce70a571a1846d244775002c2c16dff
Kenny Tzeng <kenny29282000@gmail.com>
2017-06-04 23:56:24 +0800

    init

---
+++ mli.py
@@ -0,0 +1,57 @@
+# -----------------------------------------------------------------------------
+# mli.py
+#
+# A miniLisp interpreter.
+# -----------------------------------------------------------------------------\
+
+import ply.lex as lex
+import ply.yacc as yacc
+
+tokens = (
+			'NAME','NUMBER','PLUS','MINUS','TIMES','DIVIDE',
+			'MOD','GREATER','SMALLER','EQUALS','LPAREN','RPAREN',
+			'AND','OR','NOT',
+		)
+
+# Lex Tokens
+
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_MOD	  = r'mod'
+t_GREATER = r'>'
+t_SMALLER = r'<'
+t_EQUALS  = r'='
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_AND	  = r'and'
+t_OR	  = r'or'
+t_NOT	  = r'not'
+
+def t_NUMBER(t):
+	r'\d+'
+	t.value = int(t.value)
+	return t
+
+# Precedence
+precedence = ()	
+
+# Yacc Grammar
+def p_expression(p):
+	'''expression : GREATER
+				  | AND'''
+	print(p[1])
+	
+def p_error(p):
+	print("Syntax error at '%s'" % p.value)
+
+lex.lex()
+yacc.yacc()
+
+while True:
+	try:
+		s = raw_input('>>> ')
+	except EOFError:
+		break
+	yacc.parse(s)
