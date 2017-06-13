d25ab91687fafe4efd6eb98575773afb4a8a3040
Kenny Tzeng <kenny29282000@gmail.com>
2017-06-14 00:52:42 +0800

    backup4

--- mli.py
+++ mli.py
@@ -15,7 +15,8 @@ reserved = {
 	'define' : 'DEFINE',
 	'fun' : 'FUN',
 	'if' : 'IF',
-	'print-num' : 'print_num'
+	'print-num' : 'print_num',
+	'print-bool' : 'print_bool'
 }
 
 tokens = [
@@ -68,54 +69,141 @@ def p_program(p):
 	'program : stmt'
 	print("success !!")
 
-def p_stmt_1(p):
+#STMT
+def p_stmt_exp(p):
 	'stmt : exp'
 
-def p_stmt_2(p):
+def p_stmt_print_stmt(p):
 	'stmt : print_stmt'
 
-def p_print_stmt_1(p):
+#PRINT-STMT
+def p_print_stmt_print_num(p):
 	'print_stmt : LPAREN print_num exp RPAREN'
+	if(isinstance(p[3], int)):
+		print(p[3])
+	elif(isinstance(p[3], bool)):
+		print("print-num can't print boolean value !")
 
-def p_exp_1(p):
+def p_print_stmt_print_bool(p):
+	'print_stmt : LPAREN print_bool exp RPAREN'
+	if(isinstance(p[3], bool)):
+		if(p[3]):
+			print("#t")
+		else:
+			print("#f")
+	elif(isinstance(p[3], int)):
+		print("print-bool can't print integer value !")
+
+#EXP
+def p_exp_number(p):
+	'exp : NUMBER'
+	p[0] = p[1]
+
+def p_exp_boolean_true(p):
+	'exp : TRUE'
+	p[0] = True
+
+def p_exp_boolean_false(p):
+	'exp : FALSE'
+	p[0] = False
+
+def p_exp_number_operation(p):
 	'exp : num_op'
+	p[0] = p[1]
 
+#NUM-OP
 def p_num_op_1(p):
-	'num_op : exp_plus'
+	'''num_op : exp_plus
+			  |	exp_minus
+			  | exp_times
+			  | exp_divide
+			  | exp_mod
+			  | exp_greater
+			  | exp_smaller
+			  | exp_equals'''
+	p[0] = p[1]
 
-def p_expression_plus(p):
+#EXP-PLUS
+def p_exp_plus(p):
 	'exp_plus : LPAREN PLUS exp exps RPAREN'
 	if(isinstance(p[4], list)):
 		for i in p[4]:
 			p[3] = p[3] + i
-		print(p[3])
+		# print(p[3])
+		p[0] = p[3]
 	elif(isinstance(p[4], int)):
-		print(p[3] + p[4])
+		# print(p[3] + p[4])
+		p[0] = p[3] + p[4]
 
-def p_expression_more_1(p):
+#EXP-MINUS
+def p_exp_minus(p):
+	'exp_minus : LPAREN MINUS exp exp RPAREN'
+	p[0] = p[3] - p[4]
+
+#EXP-TIMES
+def p_exp_times(p):
+	'exp_times : LPAREN TIMES exp exps RPAREN'
+	if(isinstance(p[4], list)):
+		for i in p[4]:
+			p[3] = p[3] * i
+		# print(p[3])
+		p[0] = p[3]
+	elif(isinstance(p[4], int)):
+		# print(p[3] + p[4])
+		p[0] = p[3] * p[4]
+
+#EXP-DIVIDE
+def p_exp_divide(p):
+	'exp_divide : LPAREN DIVIDE exp exp RPAREN'
+	p[0] = p[3] / p[4]
+
+#EXP-MOD
+def p_exp_mod(p):
+	'exp_mod : LPAREN MOD exp exp RPAREN'
+	p[0] = p[3] % p[4]
+
+#EXP-GREATER
+def p_exp_greater(p):
+	'exp_greater : LPAREN GREATER exp exp RPAREN'
+	p[0] = p[3] > p[4]
+
+#EXP-SMALLER
+def p_exp_smaller(p):
+	'exp_smaller : LPAREN SMALLER exp exp RPAREN'
+	p[0] = p[3] < p[4]
+
+def p_exp_equals(p):
+	'exp_equals : LPAREN EQUALS exp exps RPAREN'
+	if(isinstance(p[4], list)):
+		p[4].sort()
+		p[0] = ( p[3] == p[4][0] and p[3] == p[4][-1] )
+	elif(isinstance(p[4], int)):
+		p[0] = p[3] == p[4]
+
+#EXPS
+def p_exps_1(p):
 	'exps : exps exp'
 	if isinstance(p[1], list):
-		p[1].append(p[2])
 		p[0] = p[1]
-		print(p[0])
+		p[0].append(p[2])
+		# print(p[0])
 	elif isinstance(p[1], int):
 		p[0] = []
 		p[0].append(p[1])
 		p[0].append(p[2])
-		print(p[0])
-def p_expression_more_2(p):
+		# print(p[0])
+
+def p_exps_2(p):
 	'exps : exp'
 	p[0] = p[1]
 	print(p[0])
 
-def p_expression(p):
-	'exp : NUMBER'
-	p[0] = p[1]
-
+#Yacc error
 def p_error(p):
 	# print("Syntax error at '%s'" % p.value)
 	print("yacc error")
 
+#
 lex.lex()
 yacc.yacc()
 
